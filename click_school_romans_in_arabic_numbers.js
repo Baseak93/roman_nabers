6c5b59cba3050c87aada5a0f0f37f7c665808e1e
Berzan Gabriel <pivalux@gmail.com>
2023-02-27 09:46:41 +0200

    TotalWar

---
+++ click_school_romans_in_arabic_numbers.js
@@ -0,0 +1,30 @@
+function romanToArabic(romanNumeral) {
+	const romanNumerals = {
+		I: 1,
+		V: 5,
+		X: 10,
+		L: 50,
+		C: 100,
+		D: 500,
+		M: 1000
+	};
+  
+	let result = 0;
+  
+	for (let i = 0; i < romanNumeral.length; i++) {
+		const currentCharValue = romanNumerals[romanNumeral[i]];
+		const nextCharValue = romanNumerals[romanNumeral[i + 1]];
+  
+		if (nextCharValue && nextCharValue > currentCharValue) {
+			result += nextCharValue - currentCharValue;
+			i++;
+		} else {
+			result += currentCharValue;
+		}
+	}
+  
+	return result;
+}
+
+console.log(romanToArabic("MCMXCIV")); // 1994
+
